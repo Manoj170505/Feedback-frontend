9754eace14578198c9651e6359c1658c04f51a52
Manoj M <manojfa4451e@gmail.com>
2025-11-11 14:58:36 +0530

    Edited router

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Feedback-frontend">
       <Routes>
         {/* public routes */}
         <Route path="/" element={<Login />} />
